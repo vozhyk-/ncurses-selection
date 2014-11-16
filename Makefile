e82e1398d1d577e94b7198130c1fa26c14c3da0d
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2014-11-16 19:55:15 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+
+CFLAGS += -std=c99
+LDFLAGS += -lncurses
+
+all: sel
---
+++ sel.c
@@ -0,0 +1,76 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <ncurses.h>
+
+char *read_line(FILE *stream);
+
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	char *line;
+	char *lines[20];
+	int is_selected[20] = {0};
+	int num_lines = 0;
+	int ch;
+	int pos = 0, last_pos = 0;
+	
+	// read menu entries
+	while ((line = read_line(stdin)) != NULL)
+		lines[num_lines++] = line;
+
+	freopen("/dev/tty", "r", stdin);
+	initscr();
+	cbreak();
+	noecho();
+	keypad(stdscr, TRUE);
+
+	for (int i = 0; i < num_lines; i++)
+		printw("   %s", lines[i]);
+	refresh();
+
+	do {
+		mvchgat(last_pos, 0, -1, A_NORMAL, 0, NULL);
+		mvchgat(pos, 0, -1, A_REVERSE, 0, NULL);
+		move(pos, 1);
+		refresh();
+
+		last_pos = pos;
+
+		ch = getch();
+		switch (ch) {
+		case KEY_UP:
+			pos--;
+			if (pos < 0) pos = num_lines - 1;
+			break;
+		case KEY_DOWN:
+			pos++;
+			if (pos >= num_lines) pos = 0;
+			break;
+		case ' ':
+			is_selected[pos] = 1 - is_selected[pos];
+			mvprintw(pos, 1, is_selected[pos] ? "*" : " ");
+			break;
+		default:
+			fprintf(stderr, "%o\n", ch);
+		}
+	} while (ch != 'q' && ch != '\n');
+
+	endwin();
+
+	for (int i = 0; i < num_lines; i++)
+		if (is_selected[i])
+			fputs(lines[i], stderr);
+
+	return 0;
+}
+
+char *read_line(FILE *stream)
+{
+	char *line = NULL;
+	size_t buffer_len = 0;
+	
+	ssize_t len = getline(&line, &buffer_len, stream);
+
+	return len != -1 ? line : NULL;
+}
